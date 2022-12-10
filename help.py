255222f757a9cf178c6ada116a0f0f6e09434cc0
AndreasFranzSchweizer <90528060+AndreasFranzSchweizer@users.noreply.github.com>
2022-12-10 15:26:43 +0100

    Create help.py

---
+++ help.py
@@ -0,0 +1,7 @@
+def is_connection_entry(text):
+    if "New connection from" in text:
+        return True
+    return False
+
+assert is_connection_entry("1668755969: New connection from 2001:470:1:c84::11:4014 on port 1883.") == True
+assert is_connection_entry("1668755969: New connectionfrom 2001:470:1:c84::11:4014 on port 1883.") == False
