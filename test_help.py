9da8f65a0d2c084117e4eff8895f532c1665b320
AndreasFranzSchweizer <90528060+AndreasFranzSchweizer@users.noreply.github.com>
2022-12-10 17:27:31 +0100

    Update test_help.py

--- test_help.py
+++ test_help.py
@@ -1,5 +1,9 @@
+from help import test_remove_port
 from help import test_is_connection_entry
+from help import test_get_client_ip
 
 
 def test():
+    test_remove_port()
     test_is_connection_entry()
+    test_get_client_ip()
